3128911e8ca691f7d465b17e2b53b39ec074c018
Your Name <rob@i.tv>
2012-09-14 20:28:46 -0600

    multiple comics

--- js/app.js
+++ js/app.js
@@ -1,49 +1,55 @@
 var app = angular.module('app', ['ngResource'], function($routeProvider) {
 
   $routeProvider.when('/comic/:comicId', {
-    templateUrl : 'partials/comic.html',
-    controller : ComicController
+    templateUrl : 'partials/comics.html'
   })
 
   $routeProvider.otherwise({
-    redirectTo : '/comic/1'
+    redirectTo : '/comic/5'
   })
 })
 
-
-function ComicController ($scope, $routeParams, Comics) {
+function ComicsController ($scope, $routeParams) {
   var comicId = parseInt($routeParams.comicId, 10)
-  $scope.comicId = comicId
   $scope.previousId = Math.max(comicId - 1, 0)
   $scope.nextId = comicId + 1
 
-  Comics.query(comicId, function (comic) {
-    $scope.comic = comic
+  $scope.list = [-2, -1, 0, 1, 2].map(function (adjustment) {
+    return comicId + adjustment
+  })
+}
 
+function ComicController ($scope, $routeParams, Comics) {
+  Comics.query($scope.comicId, function (comic) {
+    $scope.comic = comic
   })
 }
 
 
 var jsonp = (function () {
   var next = 0
-  return {
-    getHandler: function (cb) {
-      var id = "jsonpHandlers" + next
-      window[id] = function () {
-        cb.apply(null, arguments)
-        delete window[id]
-      }
-      next++
-      return id
+  return function (cb) {
+    var id = "jsonpHandlers" + next
+    window[id] = function () {
+      cb.apply(null, arguments)
+      delete window[id]
     }
+    next++
+    return id
   }
 }())
 
 
 app.factory("Comics", function ($http, $resource) {
+  var cache = {}
   return {
     query: function (comicId, cb) {
-      $http.jsonp("http://dynamic.xkcd.com/api-0/jsonp/comic/" + comicId + "?callback=" + jsonp.getHandler(cb))
+      if (cache[comicId]) return cb(cache[comicId])
+      console.log("fetch", comicId)
+      $http.jsonp("http://dynamic.xkcd.com/api-0/jsonp/comic/" + comicId + "?callback=" + jsonp(function (comic) {
+        cache[comicId] = comic
+        cb(comic)
+      }))
     }
   }
 })
