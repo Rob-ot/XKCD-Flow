4e4dd08015958560f04480826e8ea1b5dfe431ce
Your Name <rob@i.tv>
2012-09-14 18:41:59 -0600

    fix err and jsonp it

--- js/app.js
+++ js/app.js
@@ -11,18 +11,22 @@ var app = angular.module('app', ['ngResource'], function($routeProvider) {
 })
 
 
-function ComicController ($scope, $routeParams) {
-  $scope.comicId = $routeParams.comicId
-  $scope.previousId = $routeParams.comicId - 1
-  $scope.nextId = $routeParams.comicId + 1
+function ComicController ($scope, $routeParams, Comics) {
+  var comicId = parseInt($routeParams.comicId, 10)
+  $scope.comicId = comicId
+  $scope.previousId = Math.max(comicId - 1, 0)
+  $scope.nextId = comicId + 1
 
+  Comics.query(function () {
+
+  })
 }
 
 
-app.factory("Comics"), function ($http, $resource) {
-  var Comics = $resource("http://dynamic.xkcd.com/api-0/jsonp/comic/1", {
-    get: {method: "JSONP"}
-  })
+app.factory("Comics", function ($http, $resource) {
+  var Comics = $resource("http://dynamic.xkcd.com/api-0/jsonp/comic/1",
+    {callback:'JSON_CALLBACK'},
+    {query:{method:'JSONP'}})
 
   return Comics
-}
+})
